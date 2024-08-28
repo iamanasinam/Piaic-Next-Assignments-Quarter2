c5454f3557b2a9db2f9b1d2b336cffa3dec0a832
iamanasinam <iamanasinam@gmail.com>
2024-08-28 23:35:15 +0500

    navbar list added with the updated fonts

---
+++ task-2-design-figma/src/app/components/navbar/list.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const List = () => {
+    return (
+        <ul className='flex justify-center items-center gap-x-10'>
+            <li>
+                <a href="" className="font-sora font-normal text-[16px] leading-[20.16px]">Female</a>
+            </li>
+            <li>
+                <a href="" className="font-sora font-normal text-[16px] leading-[20.16px]">Male</a>
+            </li>
+            <li>
+                <a href="" className="font-sora font-normal text-[16px] leading-[20.16px]">Kids</a>
+            </li>
+            <li>
+                <a href="" className="font-sora font-normal text-[16px] leading-[20.16px]">All Products</a>
+            </li>
+        </ul>
+    );
+}
+
+export default List;
