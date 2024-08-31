7d88e491bc32fb4e482f201486b3481d36f1ab92
iamanasinam <iamanasinam@gmail.com>
2024-08-31 21:35:00 +0500

    product aded

--- task-2-design-figma/src/app/components/Products/Products.tsx
+++ task-2-design-figma/src/app/components/Products/Products.tsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import Image from 'next/image'
+import ProductA from '../../../../public/producta.png'
+import ProductB from '../../../../public/productb.png'
+import ProductC from '../../../../public/productc.png'
 
 const Products = () => {
   return (
@@ -8,7 +12,13 @@ const Products = () => {
         <h2 className='promotion-h2 text-3xl font-bold'>Check what we have</h2>
       </div>
 
-          </>
+      <div className='flex justify-evenly mb-8'>
+        <div><Image className='h-[350px] w-full' src={ProductA}  alt="Promotional Image"/></div>
+        <div><Image className='h-[350px] w-full' src={ProductB}  alt="Promotional Image"/></div>
+        <div><Image className='h-[350px] w-full' src={ProductC}  alt="Promotional Image"/></div>
+      </div>
+
+    </>
   )
 }
 
