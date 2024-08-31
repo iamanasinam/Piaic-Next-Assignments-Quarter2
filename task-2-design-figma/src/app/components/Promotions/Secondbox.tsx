5b1cd5ce36a7fd23c01f05c88a3abb2a86ca40c9
iamanasinam <iamanasinam@gmail.com>
2024-08-31 19:02:33 +0500

    second bx added

---
+++ task-2-design-figma/src/app/components/Promotions/Secondbox.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Secondbox = () => {
+  return (
+    <>
+    <div className='second-box col-span-4 h-[150px] w-full flex justify-center items-center text-white'>
+      <div>
+        <h1 className='flex justify-center text-3xl font-bold'>GET 30% Off</h1>
+        <p className='flex justify-center text-xs text-white text-opacity-60 pt-3 pb-1 font-light'>USE PROMO CODE</p>
+        <div className='divbox flex justify-center w-[50] h-[30] bg-gray-700'>DINEWEEKENDSALE</div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default Secondbox
